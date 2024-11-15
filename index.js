a1e102ac7eb746066ebbddf398de1a0a0ea22883
Priviledge <privilledgeblack8@gmail.com>
2024-11-15 11:29:25 +0200

    removed unused function

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
-//(1)get dom elements of  the dino div and the cactus div  
-//(2) create function to handle the jumping dino image and handle timing
-
-//checking if is ALIVE
-let isAlive = setInterval(function () {
-  let dinoJumping = parseInt(
-    window.getComputedStyle(jumpingDino).getPropertyValue("top")
-  );
-  let cactusMovingLeft = parseInt(
-    window.getComputedStyle(cactus).getPropertyValue("left")
-  );
-
-  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-    alert("game Over");
-  }
-}, 10);
-
-
-//(3) Add event listener to allow user to play game
-
-
 const dino = document.querySelector('.dino');
 const cactus = document.querySelector('.cactus');
 const game = document.querySelector('.game');
 
+
 let isJumping = false;
 let score = 0;
-
 document.addEventListener('keydown', (event) => {
   if (event.code === 'Space' && !isJumping) {
     jump();
@@ -45,8 +24,8 @@ function jump() {
 function moveCactus() {
   const cactusRect = cactus.getBoundingClientRect();
   const dinoRect = dino.getBoundingClientRect();
-
-  if (
+   
+  if ( 
     cactusRect.right > dinoRect.left &&
     cactusRect.left < dinoRect.right &&
     cactusRect.bottom > dinoRect.top &&
@@ -55,15 +34,15 @@ function moveCactus() {
     alert('Game Over! Final Score: ' + score);
     location.reload(); 
   }
-
+  
   if (cactusRect.right <= 0) {
+    console.log("cactus is oof-screan")
     score++;
     cactus.style.animation = 'none'; 
     cactus.style.right = '-30px'; 
     cactus.style.animation = 'moveCactus 2s linear infinite'; 
   }
 }
-
 setInterval(() => {
   moveCactus();
 }, 20);

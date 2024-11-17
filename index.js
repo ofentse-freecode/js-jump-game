d98234f2b5226f4037442d8c1ca3d322d970cbc8
dania <alsmandania826@gmail.com>
2024-11-18 01:01:04 +0300

    delete inner style and remove cactus if it goes out screen

--- index.js
+++ index.js
@@ -1,102 +1,83 @@
-const dino = document.querySelector(".dino");
-const cactus = document.querySelector(".cactus");
-const game = document.querySelector(".game");
+const dino = document.querySelector('.dino');
+const cactus = document.querySelector('.cactus');
+const game = document.querySelector('.game');
 
 let gamestarted = false;
 let isJumping = false;
-let score = 0;
-
-
-function randomCacti() {
-  const cacti = [
-    'images/cactus (1).png',
-    'images/cactus (2).png',
-    'images/cactus (3).png'
-  ];
-  const numberOfCacti = Math.floor(Math.random() * cacti) + 1;
-}
-
+let score = 0 ;
 
 
 
 function jump() {
-  if (!isJumping) {
-    isJumping = true;
-    dino.classList.add("jump");
+  isJumping = true;
+  dino.style.animation = 'jump 0.5s ease-out forwards';
 
-    setTimeout(() => {
-      dino.classList.remove("jump");
-      isJumping = false;
-      score++;
-    }, 500);
-  }
+  setTimeout(() => {
+    dino.style.animation = '';
+    isJumping = false;
+
+    score++;
+  }, 300); 
 }
 
 function moveCactus() {
   const cactusRect = cactus.getBoundingClientRect();
   const dinoRect = dino.getBoundingClientRect();
-
-  if (
+   
+  if ( 
     cactusRect.right > dinoRect.left &&
     cactusRect.left < dinoRect.right &&
     cactusRect.bottom > dinoRect.top &&
     cactusRect.top < dinoRect.bottom
   ) {
-    alert("Game Over! Final Score: " + score * 0.5 * 1000 + " points");
-    location.reload();
+    alert('Game Over! Final Score: ' + score * 0.5 * 1000 + ' points');
+    location.reload(); 
   }
-
+  
   if (cactusRect.right <= 0) {
-    cactus.style.animation = "none";
-    cactus.style.right = "-30px";
-    cactus.style.animation = "moveCactus 2s linear infinite";
-  }
-  if (cactusRect.right <= 0) {
-    cactus.remove(); 
+   cactus.remove();
   }
 }
-
-function setupInputListeners() {
-  if (window.innerWidth > 768) {
+function setupInputListners(){
+  if(window.innerWidth > 768){
     document.removeEventListener("touchstart", onTouchStart);
-    document.addEventListener("keydown", onKeyDown);
+    document.addEventListener("keydown", onkeyDown);
   } else {
-    document.removeEventListener("keydown", onKeyDown);
+    document.removeEventListener("keydown", onkeyDown);
     document.addEventListener("touchstart", onTouchStart);
   }
 }
-
-function onKeyDown(event) {
-  if (event.code === "Space") {
-    if (!gamestarted) {
+function onkeyDown(event){
+  if(event.code === "Space"){
+    if(!gamestarted){
       gamestarted = true;
-      if (!cactus.classList.contains("move")) {
-        cactus.classList.add("move");
+      if(!cactus.classList.contains("move")){
+        cactus.classList.add("move")
       }
     }
   }
-  if (!isJumping) {
+  if ( !isJumping) {
     jump();
   }
 }
 
-function onTouchStart(event) {
-  if (!gamestarted) {
+function onTouchStart(event){
+  if(!gamestarted){
     gamestarted = true;
-    if (!cactus.classList.contains("move")) {
-      cactus.classList.add("move");
+    if(!cactus.classList.contains("move")){
+      cactus.classList.add("move")
     }
   }
-  jump();
+  jump()
 }
-
-setupInputListeners();
-window.addEventListener("resize", setupInputListeners);
+setupInputListners();
+window.addEventListener("resize", setupInputListners)
 
 setInterval(() => {
   moveCactus();
 }, 20);
 
 setInterval(() => {
-  console.log("Score: " + score);
+  console.log('Score: ' + score);
 }, 1000);
+
